683bc4a187828a3a0395f3742556d76467ba8f55
Pathum Vimukthi <104794593+Pathum-Vimukthi@users.noreply.github.com>
2024-09-24 23:05:20 +0530

    Refactor SideNavBar component, update navigation link handling

--- my-portfolio/src/components/SideNavBar.jsx
+++ my-portfolio/src/components/SideNavBar.jsx
@@ -20,7 +20,7 @@ function SideNavBar() {
       />
       {nav ? (
         <div className="fixed w-full h-screen bg-white/80 flex flex-col justify-center items-center z-20">
-          <a
+          <a onClick={handleNav}
             href="#main"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-100"
           >
